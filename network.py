79dfbdb8c135ee18117c50d23c4dc908e852cd16
lekicmilos <132852157+lekicmilos@users.noreply.github.com>
2023-06-03 19:48:27 +0200

    Update network.py

--- network.py
+++ network.py
@@ -4,7 +4,7 @@ import socket
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.host = "192.168.0.16" 
+        self.host = "localhost" 
         self.port = 5555
         self.addr = (self.host, self.port)
         self.id = self.connect()
